38cb7554d216ad422800c44ed13f101e944c194b
pdmoore <pdmoore@gmail.com>
2019-05-22 10:18:30 -0700

    failing test changed to cover other branch

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,14 +1,19 @@
 import com.microsoft.demo.Demo;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MyTest {
     @Test
     public void test_method_1() {
         Demo d = new Demo();
-        d.DoSomething(true);
+        String actual = d.DoSomething(true);
+        Assert.assertEquals("I am covered", actual);
     }
 
     @Test
     public void test_method_2() {
+        Demo d = new Demo();
+        String actual = d.DoSomething(false);
+        Assert.assertEquals("I am not covered", actual);
     }
 }
