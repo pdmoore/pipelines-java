a98d79b94949a2c52c08af0a1950096946ca24c1
pdmoore <pdmoore@gmail.com>
2019-05-24 16:46:35 -0700

    add test that should increase coverage a lot

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -7,8 +7,16 @@ public class GildedRoseTest {
     @Test
     public void itemQuality_should_decreaseByOne() {
         Item[] items = new Item[] { new Item("foo", 10, 20) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals(19, app.items[0].quality);
+        GildedRose sut = new GildedRose(items);
+        sut.updateQuality();
+        assertEquals(19, sut.items[0].quality);
+    }
+
+    @Test
+    public void agedBrieQuality_should_increaseByOne() {
+        Item[] items = new Item[] { new Item("Aged Brie", 10, 20) };
+        GildedRose sut = new GildedRose(items);
+        sut.updateQuality();
+        assertEquals(21, items[0].quality);
     }
 }
